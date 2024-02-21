84a5d2568f55ba0eba490854645e94b8550e8f6e
Rizwan Mehmood Bilal <105595882+bilal426786@users.noreply.github.com>
2024-02-21 19:02:34 +0500

    Structuring NavBar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,6 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div className="flex min-h-screen content-center justify-center">
-      <UserButton afterSignOutUrl="/" />
       <h1 className="font-bold text-primary-500">This is Home Page</h1>
       <h1>Home</h1>
     </div>
