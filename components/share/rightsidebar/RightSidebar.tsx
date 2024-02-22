84ab291915b2427a32753c945e91526e2024fcc0
Rizwan Mehmood Bilal <105595882+bilal426786@users.noreply.github.com>
2024-02-22 23:45:32 +0500

    signup login icon setup

---
+++ components/share/rightsidebar/RightSidebar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const RightSidebar = () => {
+  return (
+    <div className="background-light900_dark200 text-dark400_light900 fixed right-0 hidden h-screen w-[352px] overflow-y-auto px-4 pt-36 xl:block"></div>
+  );
+};
+
+export default RightSidebar;
